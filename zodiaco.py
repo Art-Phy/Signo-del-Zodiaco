4fbd48cb9c2ee27e879f0ce2dba6bf0ef3284ab8
ArturoGit <arturosanma@gmail.com>
2024-09-30 16:13:07 +0200

    developing v.2

--- zodiaco.py
+++ zodiaco.py
@@ -1,14 +1,21 @@
 ### Tu Signo del Zodiaco ###
 
-# creamos una función dónde se indican las fechas de los signos del zodiaco y cuáles son
+# creamos una función dónde se indican las fechas de inicio de los signos del zodiaco.
 def signos_zodiaco(día, mes):
-    fechas = [(21, 3), (20, 4), (21, 5), (21, 6), (23, 7), (23, 8), (23, 9), (23, 10),
-              (22, 11), (22, 12), (21, 1), (20, 2)]
+    fecha_inicio = [21, 20, 21, 21, 22, 21, 23, 23, 23, 23, 22, 22]
     signos = ['aries', 'tauro', 'géminis', 'cáncer', 'leo', 'virgo', 'libra', 'escorpio',
               'sagitario', 'capricornio', 'acuario', 'piscis']
-    
+    # determinamos el signo
+    if día < fecha_inicio[mes - 1]: # si el día es menor al inicio de ese signo
+        signo = signos[mes -2] # signo del mes anterior
+    else:
+        signo = signos[mes -1] # signo del mes actual
+
+    return (signo)
+
+# entrada del usuario
 día = int(input("Ingresa el día en que naciste: "))
 mes = int(input("Ingresa el mes: "))
 
-signo = signos_zodiaco(día, mes)
-print(f"Tu signo del zodiaco es: ")
+# mostrar el resultado
+print(f"Tu signo del zodiaco es: ", signos_zodiaco(día, mes))
